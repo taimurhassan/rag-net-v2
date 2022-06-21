8bdeb70dfe2e2d6648e21694bb0040c9daa26f8f
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-06-21 15:12:14 +0400

    Add files via upload

---
+++ Test_AF/MNIST_GITHUB.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense , Activation, Dropout
+from tensorflow.keras.optimizers import Adam ,RMSprop
+from tensorflow.keras import  backend as K
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.utils import to_categorical, plot_model
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+unique, counts = np.unique(y_train, return_counts=True)
+num_labels = len(np.unique(y_train))
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+image_size = x_train.shape[1]
+input_size = image_size * image_size
+
+x_train = np.reshape(x_train, [-1, input_size])
+x_train = x_train.astype('float32') / 255
+x_test = np.reshape(x_test, [-1, input_size])
+x_test = x_test.astype('float32') / 255
+
+batch_size = 128
+hidden_units = 256
+dropout = 0.45
+
+model = Sequential()
+model.add(Dense(hidden_units, input_dim=input_size))
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(hidden_units))
+model.add(Activation('relu'))
+model.add(Dropout(dropout))
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=2, batch_size=batch_size)
+loss, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+print("\nTest accuracy: %.1f%%" % (100.0 * acc))
