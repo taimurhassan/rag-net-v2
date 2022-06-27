b856753bf9b39c754958a1d09a2252b4bc8ef196
Taimur Hassan <106734296+Abdelfatah1998@users.noreply.github.com>
2022-06-27 05:00:10 +0400

    EXP Test Backet7

--- Test_AF/TRainCOVID.py
+++ Test_AF/TRainCOVID.py
@@ -23,9 +23,8 @@ from pathlib import Path
 import pandas as p
 import pickle
 
-# task_dataset_folder = os.environ.get("ICHOR_INPUT_DATASET", "/COMPASS-XP")
-task_dataset_folder = 'ICHOR_INPUT_DATASET'
-file = pickle.load(open(os.path.join(task_dataset_folder, "COMPASS-XP"), "rb"))
+task_dataset_folder = os.environ.get("ICHOR_INPUT_DATASET")
+file = pickle.load(open(os.path.join(task_dataset_folder, "COMPASS-XP/"), "rb"))
 
 train_dir = pickle.load(file)
 
