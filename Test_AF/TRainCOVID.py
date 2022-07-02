f0bdf1c4ff34ae2c0bf1b721cbb659c315be81da
Taimur Hassan <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-02 08:29:23 +0400

    EXP Test Dataset4

--- Test_AF/TRainCOVID.py
+++ Test_AF/TRainCOVID.py
@@ -33,7 +33,7 @@ dangerous_dir = Path(os.environ["ICHOR_INPUT_DATASET"]) / "COMPASS-XP" / "Danger
 
 for dirs in os.listdir(dangerous_dir):
     for image_file in os.listdir(dangerous_dir):
-        im = Image.open(dangerous_dir / image_file, 'r', encoding="utf-8")
+        im = Image.open(dangerous_dir / image_file, 'r', "utf-8")
         print(f'Processing for dir {dirs}, file {image_file}.')
 print(im)
 
