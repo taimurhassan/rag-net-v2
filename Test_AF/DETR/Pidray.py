8e8a3fb650792306083ef31911bc6798388c53e5
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-05 19:11:48 +0400

    khaled

--- Test_AF/DETR/Pidray.py
+++ Test_AF/DETR/Pidray.py
@@ -17,7 +17,9 @@ from detr_tf.optimizers import setup_optimizers
 from detr_tf import training
 from detr_tf.inference import get_model_inference, numpy_bbox_to_image
 
- 
+
+
+
 class TrainConfig(TrainingConfig):
     def __init__(self):
         super().__init__()
