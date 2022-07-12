2f0f6104cf50aba0765e8dff8fc0181c9471bedc
Taimur Hassan <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-12 20:20:59 +0400

    EXP Test

--- Test_AF/DETR/Pidray.py
+++ Test_AF/DETR/Pidray.py
@@ -25,7 +25,7 @@ class TrainConfig(TrainingConfig):
         self.data = DataConfig(data_dir=self.data_dir, img_dir="train", ann_file="annotations/xray_train.json")
         self.image_size = (500, 500)
         # self.normalized_method = 'tf_resnet'
-
+ 
 train_config = TrainConfig()
 train_iterator, class_names = load_coco_dataset(train_config, train_config.batch_size, augmentation=True)
 
