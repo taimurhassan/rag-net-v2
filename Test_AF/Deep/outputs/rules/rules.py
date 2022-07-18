c71150394647cf79805ea00b5295828a5c11d37c
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-18 15:32:53 +0400

    Delete rules.py

--- Test_AF/Deep/outputs/rules/rules.py
+++
@@ -1,7 +0,0 @@
-def findDecision(obj): #obj[0]: distance
-	# {"feature": "distance", "instances": 4932, "metric_value": 0.0, "depth": 1}
-	if obj[0]>1.0187:
-		return 'Yes'
-	elif obj[0]<=1.0187:
-		return 'Yes'
-	else: return 'Yes'
