d83c2907c85c211191e3c20c2d15f94d39798838
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-21 11:43:27 +0400

    EXP AS

--- Test_AF/Deep/te.py
+++ Test_AF/Deep/te.py
@@ -10,6 +10,7 @@ Path(os.environ["ICHOR_LOGS"]).mkdir(exist_ok=True, parents=True)
 
 
 fil = Path(os.environ["ICHOR_OUTPUT_DATASET"]) / "dict2.json"
+fil1 = Path(os.environ["ICHOR_OUTPUT_DATASET"]) / "dict3.json"
 
 
 # Data to be written
@@ -21,4 +22,18 @@ dictionary = {
 }
  
 with open(fil, "w") as outfile:
+    json.dump(dictionary, outfile)
+
+
+
+
+# Data to be written
+dictionary = {
+    "name": "sathiyajith",
+    "rollno": 56,
+    "cgpa": 8.6,
+    "phonenumber": "9976770500"
+}
+ 
+with open(fil1, "w") as outfile:
     json.dump(dictionary, outfile)
