5de41340bb2b2317fa2c22e21522c9764a90bb90
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-22 23:14:04 +0400

    EXP MN

--- Test_AF/Deep/te.py
+++ Test_AF/Deep/te.py
@@ -17,16 +17,29 @@ fil = Path(os.environ["ICHOR_OUTPUT_DATASET"]) / "new" / "di.json"
 with open(fil, 'w') as f:
     print("The json file is created")
 
-# fil1 = Path(os.environ["ICHOR_OUTPUT_DATASET"]) / "new" / "dict3.json"
 
-# # Data to be written
-# dictionary = {
-#     "name": "sathiyajith",
-#     "rollno": 56,
-#     "cgpa": 8.6,
-#     "phonenumber": "9976770500"
-# }
+# Data to be written
+dictionary = {
+    "name": "sathiyajith",
+    "rollno": 56,
+    "cgpa": 8.6,
+    "phonenumber": "9976770500"
+}
 
+# load json module
+# import json
+
+# create json object from dictionary
+json = json.dumps(dictionary)
+
+# open file for writing, "w" 
+f = open(fil,"w")
+
+# write json object to file
+f.write(json)
+
+# close file
+f.close()
 # with open(fil, "w") as outfile:
 #     json.dump(dictionary, outfile)
 
