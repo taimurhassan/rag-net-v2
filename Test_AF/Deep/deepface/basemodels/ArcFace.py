187e57a2261fc0aa9655fcd521d8693fd3dc80be
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-07-26 18:32:27 +0400

    EXP AMLT

--- Test_AF/Deep/deepface/basemodels/ArcFace.py
+++ Test_AF/Deep/deepface/basemodels/ArcFace.py
@@ -30,11 +30,11 @@ def loadModel(url = 'https://github.com/serengil/deepface_models/releases/downlo
 
 	home = functions.get_deepface_home()
 
-	file_name = "rag-net-v2/Test_AF/Deep/deepface/basemodels/arcface_weights.h5"
-	# output = home+'/.deepface/weights/'+file_name
+	file_name = "arcface_weights.h5"
+	output = home+'/.deepface/weights/'+file_name
 	# output = Path(os.environ["ICHOR_INPUT_DATASET"]) / "DFweights" / file_name
 	# output = '/mnt/datasets/rag-net-v2-0c6f96b8050c43fd-outputs/output/DFWeights/' + file_name
-	output = file_name
+	# output = file_name
 
 	# if os.path.isfile(output) != True:
 
