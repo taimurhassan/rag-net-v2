af196c9388e1ab6a6df064b331d085b45fd77f18
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-08-17 09:38:30 +0400

    EXP aa

--- Test_AF/deepface/Active_Even_L2_DeepFace.py
+++ Test_AF/deepface/Active_Even_L2_DeepFace.py
@@ -5267,6 +5267,14 @@ print("P55 Done")
 
 # fil = '/mnt/datasets/rag-net-v2-0c6f96b8050c43fd-outputs/output/DeepFaceActiveEvenL2/dict55.json'
 
+# fil = 'output/DeepFaceActiveEvenL2/dict55.json'
+# s3_client.download_file(
+#     "rag-net-v2-0c6f96b8050c43fd-outputs",
+#     fil,
+#     "/app/output/dict55.json"
+# )
+
+
 fil = 'output/DeepFaceActiveEvenL2/dict55.json'
 s3_client.download_file(
     "rag-net-v2-0c6f96b8050c43fd-outputs",
