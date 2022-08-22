5b3bfc7d46470e49df63d4f390a44e96a7e6385c
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-08-22 20:35:12 +0400

    EXP AEL1 Dlib

--- Test_AF/deepface/Active_Even_L1_New_Dlib.py
+++ Test_AF/deepface/Active_Even_L1_New_Dlib.py
@@ -2317,8 +2317,8 @@ download_files(
     dir="droneSURF/Active_Even_L1/23/"
 )
 
-dataset_path = "/app/input/"
 
+dataset_path = "/app/input/"
 
 negatives.file_x = dataset_path + negatives.file_x
 negatives.file_y = dataset_path + negatives.file_y
