45b8789cb3a78255547dbbe5d2f4093223cc7c75
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-09-07 09:27:42 +0400

    EXP AEL2 PN

--- Test_AF/deeptorch/deep/DeepFace.py
+++ Test_AF/deeptorch/deep/DeepFace.py
@@ -26,7 +26,7 @@ from torchvision.transforms import ToTensor, Resize
 # from deepface.basemodels import ArcFace, Boosting, VGGFace
 # from deep.basemodels import ShuffleFaceNet, FaceNet, MobileFaceNet, ArcFace, PocketNet, ElasticFace, MixFaceNetXS, MixFaceNetM, Boosting, VGGFace
 from deep.basemodels import PocketNet, Boosting
-from deep.commons import functions, realtime, distance as dst
+from deep.commons import functions, distance as dst
 
 import tensorflow as tf
 tf_version = int(tf.__version__.split(".")[0])
@@ -659,39 +659,6 @@ def represent(img_path, model_name = 'PacketNet', model = None, enforce_detectio
 	# embedding = model.predict(img)[0].tolist()
 
 	return embedding
-
-def stream(db_path = '', model_name ='PacketNet', detector_backend = 'opencv', distance_metric = 'cosine', enable_face_analysis = True, source = 0, time_threshold = 5, frame_threshold = 5):
-
-	"""
-	This function applies real time face recognition and facial attribute analysis
-
-	Parameters:
-		db_path (string): facial database path. You should store some .jpg files in this folder.
-
-		model_name (string): VGG-Face, Facenet, OpenFace, DeepFace, DeepID, Dlib or Ensemble
-
-		detector_backend (string): opencv, ssd, mtcnn, dlib, retinaface
-
-		distance_metric (string): cosine, euclidean, euclidean_l2
-
-		enable_facial_analysis (boolean): Set this to False to just run face recognition
-
-		source: Set this to 0 for access web cam. Otherwise, pass exact video path.
-
-		time_threshold (int): how many second analyzed image will be displayed
-
-		frame_threshold (int): how many frames required to focus on face
-
-	"""
-
-	if time_threshold < 1:
-		raise ValueError("time_threshold must be greater than the value 1 but you passed "+str(time_threshold))
-
-	if frame_threshold < 1:
-		raise ValueError("frame_threshold must be greater than the value 1 but you passed "+str(frame_threshold))
-
-	realtime.analysis(db_path, model_name, detector_backend, distance_metric, enable_face_analysis
-						, source = source, time_threshold = time_threshold, frame_threshold = frame_threshold)
 
 def detectFace(img_path, target_size = (112, 112), detector_backend = 'opencv', enforce_detection = True, align = True):
 
