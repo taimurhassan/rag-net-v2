2a0b07bbb96bc56eb9f9a54b9f45fe6e5ba90825
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-09-24 15:37:44 +0400

    EXP AML2 Face

--- Test_AF/deeptorch/Active_Morn_L2_Face.py
+++ Test_AF/deeptorch/Active_Morn_L2_Face.py
@@ -25,7 +25,7 @@ def download_files(s3_client, bucket_name, local_path, file_names, dir):
             str(file_path)
         )
 
-model_name = "Facenet"
+model_name = "FaceNet"
 distance_metric = "euclidean_l2"
 detector_backend = 'opencv'
 
