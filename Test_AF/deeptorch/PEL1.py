7ddba48340c6eba44caa581b2dc7ffd572b2c562
Abdelfatah1998 <106734296+Abdelfatah1998@users.noreply.github.com>
2022-11-17 16:21:03 +0400

    EXP PEL1 XS

--- Test_AF/deeptorch/PEL1.py
+++ Test_AF/deeptorch/PEL1.py
@@ -564,14 +564,14 @@ for img in image:
         download_files(
             s3_client,
             "rag-net-v2-0c6f96b8050c43fd-inputs",
-            "/app/input",
+            "/app/input/" + str(i),
             file_names=id,
             dir="droneSURF/Passive_Even_L1/"+str(i)+"/"
             )
 
             
-        if not os.path.exists("/app/input"):
-            os.makedirs("/app/input")
+        if not os.path.exists("/app/input/" + str(i)):
+            os.makedirs("/app/input/" + str(i))
 
 
         download_files(
@@ -589,7 +589,7 @@ for img in image:
 
         Gallery_Images = '/app/input/gallery/'+img
         # Probe_Images = '/app/input/'+str(i)+'/'
-        Probe_Images = '/app/input/'
+        Probe_Images = '/app/input/' + str(i)
 
         model_name = 'MixFaceNet'
         detector_backend = 'opencv'
